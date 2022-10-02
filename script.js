8d01aa22ed4605b0a0df1dada54bfa33039acf82
nikkikhou <nikkikhou@gmail.com>
2022-10-02 12:02:14 -0400

    created a generate password function that allows users to choose which character they would like in their password

---
+++ script.js
@@ -0,0 +1,82 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+function generatePassword() {
+  var possibleLowerChars = "abcdefghijklmnopqrstuvwxyz";
+  var possibleUpperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var possibleNumericChars = "0123456789";
+  var possibleSpecialChars = "!@#$%^&*()";
+
+  var passwordLength = prompt(
+    "Please choose a length of between 8-128 characters",
+    8
+  );
+  if (isNaN(passwordLength)) {
+    alert("This is not a number.");
+    return;
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Your password must be between 8-128 characters.");
+    return;
+  }
+
+  var passwordLower = confirm(
+    "Should your password contain lowercase characters?",
+    true
+  );
+  var passwordUpper = confirm(
+    "Should your password contain uppercase characters?",
+    true
+  );
+  var passwordNumeric = confirm(
+    "Should your password contain numerical characters?",
+    true
+  );
+  var passwordSpecial = confirm(
+    "Should your password contain special characters?",
+    true
+  );
+
+  var finalPossibleChars = "";
+
+  if (passwordLower === true) {
+    finalPossibleChars = finalPossibleChars.concat(possibleLowerChars);
+  }
+
+  if (passwordUpper === true) {
+    finalPossibleChars = finalPossibleChars.concat(possibleUpperChars);
+  }
+
+  if (passwordNumeric === true) {
+    finalPossibleChars = finalPossibleChars.concat(possibleNumericChars);
+  }
+
+  if (passwordSpecial === true) {
+    finalPossibleChars = finalPossibleChars.concat(possibleSpecialChars);
+  }
+
+  if (finalPossibleChars.length === 0) {
+    alert
+  }
+
+  var finalPassword = "";
+
+  for (i = 0; i < passwordLength; i++) {
+    finalPassword += finalPossibleChars[Math.floor(Math.random() * finalPossibleChars.length)];
+  }
+
+  return finalPassword;
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
